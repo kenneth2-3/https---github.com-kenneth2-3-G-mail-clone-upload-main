4624e6de4335961975c888360988f84c28667bea
kenneth2-3 <deborah2020.kenneth@gmail.com>
2023-11-30 23:18:08 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("myDropdown");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('#dropdown-icon')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
+
+
+function toggleMyDropdown() {
+    var dropContent = document.getElementById("myDrop");
+    if (dropContent.style.display === "block") {
+        dropContent.style.display = "none";
+    } else {
+        dropContent.style.display = "block";
+    }
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('#dropicon')) {
+        var dropdowns = document.getElementsByClassName("dropcontent");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
+
+function toggleAsideIcons() {
+    var asideIcons = document.getElementById('#asideIcons');
+    var toggleButton = document.getElementById('#toggleButton');
+
+    if (asideIcons.style.right === '0px') {
+        asideIcons.style.right = '-250px';
+        toggleButton.innerHTML = '&chevron_right'; // Change to chevron icon
+    } else {
+        asideIcons.style.right = '0px';
+        toggleButton.innerHTML = '&chevron_right'; // Change to chevron icon
+    }
+}
+
+
+
+
+
+
+
